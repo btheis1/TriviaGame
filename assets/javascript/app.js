8c77a25640833bc150d9a45da13f91506924480e
C Howe <brook.theis@gmail.com>
2018-10-15 18:17:35 -0600

    Links to external js, css, and jQuery. Pseudo-code.

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+// CREATE 60 second timer
+    //SHOW timer on page
+    //when timer = 0, SHOW answer page
+
+// DEFINE global variables
+    // timer
+    // wins counter
+    // losses counter
+
+
+//Functions
+    //PRESENT question
+    //PRESENT answer options. Array -- buttons with different index numbers?
+        //ON CLICK
+            //COMPARE user answer to index number of correct answer. ie:
+            //IF i === 3 (correct answer),
+                 //DISPLAY screen "You win!"
+                // INCREASE wins counter by 1
+                // SET screen time out after ~ 20 seconds, DISPLAY next question
+
+             //else (wrong answer)
+                // DISPLAY screen "You lost, correct answer = x"
+                // INCREASE losses counter by 1
+                // SET screen time out after ~20 seconds, DISPLAY next question
+
+    // REPEAT for 7 questions
+        // Function to pull up next question, call it at the end of win/loss screen ?
+    // Reset function--->
+        // function around entire game, blank variables assigned at the beginning, etc. Call it once at beginning, and again ON CLICK
+        // for "play again?" button. 
+    
