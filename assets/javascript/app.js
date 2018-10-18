8be574fa20a4a910e920ee839f09334a877d7626
C Howe <brook.theis@gmail.com>
2018-10-18 13:07:33 -0600

    Created if/else for correct/incorrect answer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,66 @@
-// CREATE 60 second timer
-    //SHOW timer on page
-    //when timer = 0, SHOW answer page
-
 // DEFINE global variables
     // timer
+    var questionTimer = 60;
     // wins counter
+    var wins = 0;
     // losses counter
-
+    var losses = 0;
 
 //Functions
+
+//START game on button click
+$("#start").click(function() {
+
+    //CREATE timer
+    var questionTimer = 60;
+    var counter = setInterval(timer, 1000);
+    
+    function timer() {
+    questionTimer= questionTimer-1;
+        if (questionTimer <= 0) {
+            clearInterval(counter);
+            //counter ended, do something here
+            return;
+    }
+    //SHOW timer on page
+    $("#timer").html("Time Left: " + questionTimer);
+    }   
+    //HIDE button
+    $("#start").hide();
+
     //PRESENT question
+    $("#question").html("<h1> Question 1. Answer: C </h1>");
+
+    //PRESENT answer options.
+    
+        $("#choices").append("<button> Option A </button>"),
+        $("#choices").append("<button> Option B </button>"),
+        $("#choices").append("<button> Option C </button>"),
+        $("#choices").append("<button> Option D </button>"),
+
+
+    $("#choices button").each(function (index, button) {
+        $(button).attr("data-optionValue", index + 1);
+    })
+
+    
+
+    $("#choices button").click(function(){
+        var optionVal = $(this).attr("data-optionValue");
+
+        if (Number(optionVal) === 3) {
+            console.log("correct");
+        }else {
+            console.log("incorrect");
+        }
+    }) 
+   
+    
+})
+
+
+//when timer = 0, SHOW answer page
+
     //PRESENT answer options. Array -- buttons with different index numbers?
         //ON CLICK
             //COMPARE user answer to index number of correct answer. ie:
